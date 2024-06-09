bdd9abb7d8803b6eff6eeea6f076d67923291d09
Anastasija Draskovic <99082640+AngieDraskovic@users.noreply.github.com>
2024-06-09 23:21:21 +0200

    minor change

--- frontend/src/app/service/recommendation.service.ts
+++ frontend/src/app/service/recommendation.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class RecommendationService {
-
-  constructor() { }
+  private apiUrl = `${environment.apiHost}/recommendations/products`;
+  constructor(private http: HttpClient) { }
 }
